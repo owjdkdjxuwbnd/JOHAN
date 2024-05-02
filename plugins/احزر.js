195e105333eca7cb9b3d79a913d9f7dc95fda900
乡kặkặśĥí乡 <145879884+owjdkdjxuwbnd@users.noreply.github.com>
2024-05-02 20:20:29 +0300

    Update احزر.js

--- plugins/احزر.js
+++ plugins/احزر.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+//import axios from 'axios';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const pablo = (await axios.get(https://raw.githubusercontent.com/owjdkdjxuwbnd/JOHAN/master/src/JSON/%D8%A7%D8%AD%D8%B2%D8%B1.json)).data;
   const pabloo = await pablo[Math.floor(pablo.length * Math.random())];
