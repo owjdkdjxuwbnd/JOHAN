48a104effff2258ad6abf1e3cb6b826ebc5f683b
乡kặkặśĥí乡 <145879884+owjdkdjxuwbnd@users.noreply.github.com>
2024-05-02 21:16:08 +0300

    Update احزر.js

--- plugins/احزر.js
+++ plugins/احزر.js
@@ -1,27 +1,11 @@
-import axios from 'axios';
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let rees = (await axios.get(https://raw.githubusercontent.com/owjdkdjxuwbnd/JOHAN/master/src/JSON/%D8%A7%D8%AD%D8%B2%D8%B1.json)).data  
+let url = await rees[Math.floor(rees.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', *احزر من انا*, m)} 
 
-const handler = async (m, {conn, usedPrefix, command}) => {
-  try {
-    const response = await axios.get(https://raw.githubusercontent.com/owjdkdjxuwbnd/JOHAN/master/src/JSON/%D8%A7%D8%AD%D8%B2%D8%B1.json);
-    const data = response.data;
-    
-    const pablo = JSON.parse(data);
-    
-    if (!pablo || pablo.length === 0) {
-      return conn.reply(m.chat, 'لا يوجد بيانات متاحة', m);
-    }
-    
-    const pabloo = pablo[Math.floor(Math.random() * pablo.length)];
-    
-    conn.sendFile(m.chat, pabloo, 'error.jpg', '*احزر من انا*', m);
-  } catch (error) {
-    console.error(error);
-    conn.reply(m.chat, 'حدث خطأ أثناء معالجة الطلب', m);
-  }
-};
 
-handler.help = ['gamos', 'gamo'];
-handler.tags = ['internet'];
-handler.command = /^احزر$/i;
-
-export default handler;
+handler.help = ['messi']
+handler.tags = ['img']
+handler.command = /^(احرز)$/i
+export default handler
