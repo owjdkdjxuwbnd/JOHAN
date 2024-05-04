5f00be146dd97f4ccfce80de61322aa595b573a1
乡kặkặśĥí乡 <145879884+owjdkdjxuwbnd@users.noreply.github.com>
2024-05-05 02:25:31 +0300

    Update التعديل ترحيب.js

--- plugins/التعديل ترحيب.js
+++ plugins/التعديل ترحيب.js
@@ -3,12 +3,13 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text
-    m.reply('*تــم وضــع رســالــة الــتـرحـيب !*')
-  } else throw *أدخــل رســالــة الـتـرحــيب !*\n*عــشان تــعـمل مـنــشن أكــتب @user, عــشان تـحط أســم الـجروب أكـتب @group, عــشان تـحط وصــف أكــتب @desc*
+    m.reply('✅ The welcome message is configured')
+  } else
+    throw ✳️ Enter the Welcome message\n\n@user (mention)\n@group (Group name)\n@desc (description of group)
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
-handler.command = ['الترحيب'] 
+handler.command = ['setwelcome']
 handler.admin = true
 handler.owner = false
 
