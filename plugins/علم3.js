d1d5cc62b68f5e58cd7a38f0f067624fee7817d3
乡kặkặśĥí乡 <145879884+owjdkdjxuwbnd@users.noreply.github.com>
2024-05-05 01:46:45 +0300

    Add files via upload

--- plugins/علم3.js
+++ plugins/علم3.js
@@ -1,26 +1,25 @@
-//🧧🗿🗿🗿🗿🗿
 let timeout = 60000
 let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
     if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '⚡️┃لم يتم الاجابة علي السؤال بعد┃⚡️ ❯', conn.tebakbendera[id][0])
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/owjdkdjxuwbnd/JOHAN/master/src/JSON/%D8%A7%D8%AD%D8%B2%D8%B1.json')).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/socona12/test/main/3lm.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = *${command.toUpperCase()}*
-  ⎔↞┃الـوقـت⏱️↞ *${(timeout / 1000).toFixed(2)} ┃
-  *استخدم .انسحب للأنسحاب*
-  ⎔↞┃الـجـائـزة🎖↞ ${poin} نقاط┃
-* ┃𝑩𝑶𝑻┃↞⎔*
-     .trim()
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+ استخدم .انسحب للأنسحاب
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+『𝙕𝙊𝙍𝙊-𝘽𝙊𝙏』
+     `.trim()
     conn.tebakbendera[id] = [
-        await conn.sendFile(m.chat, json.question, '', caption, m),
+        await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, ❮ 💧┃انتهي الوقت┃💧❯\n⎔↞┃الاجـابـة✓↞ ${json.response}*┇, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
