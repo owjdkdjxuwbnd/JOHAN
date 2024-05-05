ffe1761105463c3e9878940ec00d73a077f01a74
乡kặkặśĥí乡 <145879884+owjdkdjxuwbnd@users.noreply.github.com>
2024-05-05 03:26:53 +0300

    Update علم1.js

--- plugins/علم1.js
+++ plugins/علم1.js
@@ -1,31 +1,31 @@
-import similarity from 'similarity'
-const threshold = 0.72
-export async function before(m) {
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
-    if (!m.quoted  !m.quoted.fromMe  !m.quoted.isBaileys  !m.text  !/.*•┇❐↞استخدم انسحب للانسحاب┇*/i.test(m.quoted.text) || /.*hhint/i.test(m.text))
-        return !0
-    this.tokitoki = this.tokitoki ? this.tokitoki : {}
-    if (!(id in this.tokitoki))
-        return this.reply(m.chat, '*❐┃هــاذا الــســؤال قــد انــتـهـى┃☑️❯*', m)
-    if (m.quoted.id == this.tokitoki[id][0].id) {
-        let isSurrender = /^(انسحب|surr?ender)$/i.test(m.text)
-        if (isSurrender) {
-            clearTimeout(this.tokitoki[id][3])
-            delete this.tokitoki[id]
-            return this.reply(m.chat, '*❐┃طـلـع غـبـي و انســحــب┃⚠️ ❯*', m)
-        }
-        let json = JSON.parse(JSON.stringify(this.tokitoki[id][1]))
-
-        if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tokitoki[id][2]
-            this.reply(m.chat, *❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n*❐↞┇الـجـائـزة💰↞* *${this.tokitoki[id][2]}* *نقطه┇❯*, m)
-            clearTimeout(this.tokitoki[id][3])
-            delete this.tokitoki[id]
-        } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
-            m.reply(*❐ ┃اقـربـت مـن الاجـابـه┃🚸 ❯*)
-        else
-            this.reply(m.chat, *❐┃اجـابـة خـاطـئـة ┃❌ ❯*, m)
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+        throw false
     }
-    return !0
+    let src = await (await fetch('https://raw.githubusercontent.com/owjdkdjxuwbnd/JOHAN/master/src/JSON/JSON%20%D8%B9%D9%84%D9%85%20%20.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+*❰BY : KUROSAKI🧧❱*
+     `.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.question, '', caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.response}*┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
 }
-export const exp = 0
+handler.help = ['guessflag']
+handler.tags = ['game']
+handler.command = /^علم/i
+
+export default handler
